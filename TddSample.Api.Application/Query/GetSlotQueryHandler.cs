4772533bb7ddf31566b00ddbf2369804d3aeeee4
Chandima Ranaweera <chan4lk@gmail.com>
2022-01-13 15:45:26 +0530

    Remove test project

--- TddSample.Api.Application/Query/GetSlotQueryHandler.cs
+++ TddSample.Api.Application/Query/GetSlotQueryHandler.cs
@@ -1,35 +1,14 @@
 ﻿using MediatR;
-using TddSample.Api.Application.Repository;
-using TddSample.Api.Application.Services;
 using TddSample.Domain;
 
 namespace TddSample.Api.Application.Query
 {
     public class GetSlotQueryHandler : IRequestHandler<GetSlotsQuery, IEnumerable<Slot>>
     {
-        private readonly ISlotRepository _repository;
-        private readonly IDateTimeService _dateTimeService;
-
-        public GetSlotQueryHandler(ISlotRepository repository, IDateTimeService dateTimeService)
-        {
-            _repository = repository;
-            _dateTimeService = dateTimeService;
-        }
+        
         public async Task<IEnumerable<Slot>> Handle(GetSlotsQuery request, CancellationToken cancellationToken)
         {
-            var slots = await _repository.GetAllAsync();
-
-            var result = new List<Slot>();
-
-            foreach (var slot in slots)
-            {
-                if(slot.From.Hours > _dateTimeService.Current().Hour)
-                {
-                    result.Add(slot);
-                }                
-            }
-
-            return result;
+            throw new NotImplementedException();
         }
     }
 }
